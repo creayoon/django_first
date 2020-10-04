96cde23ae7aaf561be6e4f761e80c8ff7ec782b8
creayoon <yoonssl.us@gmail.com>
2020-10-04 16:47:22 +0900

    board list paginator

--- community/board/views.py
+++ community/board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Board
 from user.models import User
 from .forms import BoardForm
@@ -36,5 +37,9 @@ def board_write(request):
     return render(request, 'board_write.html', {'form':form})
  
 def board_list(request):
-    boards = Board.objects.all().order_by('-id') # id 역순으로 정렬, 시간역순, 최신글이 최상단
+    all_boards = Board.objects.all().order_by('-id') # id 역순으로 정렬, 시간역순, 최신글이 최상단
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(all_boards, 3) # 1page당 게시글 갯수
+
+    boards = paginator.get_page(page)
     return render(request, 'board_list.html', {'boards':boards})
